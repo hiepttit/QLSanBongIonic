aba6c486f69c73bec578ecb1f8b5fe1458df5664
nthanhquoc078 <nthanhquoc078@gmail.com>
2019-12-20 23:21:09 +0700

    update check pitch

--- src/pages/booking/booking.ts
+++ src/pages/booking/booking.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, DateTime } from 'ionic-angular';
 import { PitchProvider } from '../../providers/PitchProvider';
 
 @Component({
@@ -9,16 +9,24 @@ import { PitchProvider } from '../../providers/PitchProvider';
 export class BookingPage {
   private data:any;
   private lstOrder;
-  private idSmallPitch;
+  private idSmallPitch:any=0;
+  private date:string;
+  private startTime:string="";
+  private endTime:string="";
+  private lstPitchFree;
   constructor(public navCtrl: NavController, private navParams:NavParams,public pitchProvider:PitchProvider) {
+    this.date= new Date().toISOString();
     this.data = navParams.get('data');
-    this.getListOrder();
+    // this.getListOrder();
   }
   private async getListOrder(){
-    let lst = await this.pitchProvider.getListPitchOrder();
-    this.lstOrder = lst;
+    let lst = await this.pitchProvider.getListPitchOrder(this.date,this.startTime,this.endTime);
+    this.lstPitchFree = lst;
   }
   private chooseSmallPitch(id){
     this.idSmallPitch = id;
+  }
+  private booking(){
+
   }
 }
--- src/providers/PitchProvider.ts
+++ src/providers/PitchProvider.ts
@@ -16,9 +16,15 @@ export class PitchProvider extends BaseServiceProvider {
     public async searchPitchByName(namePitch) {
         return this.getData(this.host + "BigPitch/search?name=" + namePitch, {});
     }
-    public async getListPitchOrder(id_big_pitch = null, date = Date.now()){
+    public async getListPitchOrder(date = new Date().toISOString(), startTime = null, endTime = null,id_big_pitch = null){
         //let id_big_pitch = 
-        let lstPara = "";
+        let lstPara = "date="+date+"&startTime="+startTime+"&endTime="+endTime;
         return this.getData(this.host + "smallpitch/findPitch?" + lstPara, {});
+    }
+    public async bookingPitch(parameters){
+        let para = "id_orderer="+1;
+        para+="&id_pitch="+parameters.idSmallPitch;
+        para+="&start_time="+parameters.idSmallPitch;
+        para+="&end_time="+parameters.idSmallPitch;
     }
 }
